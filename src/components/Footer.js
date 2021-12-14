83d06417ada7fa292139d5d3d3f0cf666ee09205
thiagobritto <thiagobritto.developer@gmail.com>
2021-12-14 11:05:19 -0300

    implements contact

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 export default `
     <p>
-        &copy; Copyright 2016 - 2022 All rights reserved - <a href="https://thiagobritto.github.io/">Thiago Britto</a>
+        &copy; Copyright 2016 - 2022 All rights reserved - <a href="https://thiagobritto.github.io/" target="_blank">Thiago Britto</a>
     </p>
 `
