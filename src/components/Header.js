f6f1bd09d6120ba1d85cd091654b02ce188eebe5
thiagobritto <thiagobritto.developer@gmail.com>
2022-05-24 10:18:40 -0300

    menu mobile refactory

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ export default (`
     <div class="container">
         <a href="/" id="logo">Thiago Britto</a>
         <a href="#" id="btn-menu" class="material-icons">menu</a>
-        <nav class="hide">
+        <nav class="nav-menu">
             <a href="/" id="logo-menu">Thiago Britto</a>
             <ul>
                 <li><a href="/">Home</a></li>
@@ -10,5 +10,6 @@ export default (`
                 <li><a href="/projects.html">Projects</a></li>
             </ul>
         </nav>
+        <div class="overlay-menu"></div>
     </div>
 `)
--- src/setup.js
+++ src/setup.js
@@ -2,7 +2,6 @@ import { doc } from './services/app.js'
 import http from './services/http.js'
 import Header from './components/Header.js'
 import Footer from './components/Footer.js'
-import mobileMenu from './services/renderer/mobileMenu.js'
 
 http.get('/src/config.json').then(res => {
     const { title, version } = res
@@ -12,4 +11,10 @@ http.get('/src/config.json').then(res => {
 doc.getElementById('cabecalho').innerHTML = Header
 doc.getElementById('rodape').innerHTML = Footer
 
-doc.getElementById('btn-menu').onclick = () => mobileMenu(doc)
+doc.getElementById('btn-menu').onclick = openMenu
+
+function openMenu(){
+    doc.querySelector('.nav-menu').classList.toggle('open-menu')
+    doc.querySelector('.overlay-menu').classList.toggle('open-menu')
+    doc.querySelector('.overlay-menu').onclick = openMenu
+}
