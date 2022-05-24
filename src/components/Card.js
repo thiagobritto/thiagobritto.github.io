918e09913e9c75c7e342181c1f531304ddf2fc2c
thiagobritto <thiagobritto.developer@gmail.com>
2022-05-24 12:03:24 -0300

    tradução de conteudo

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ export default data => (`
                 <h3> ${data.title} </h3>
                 <p> ${data.label} </p>
             </div>
-            <a href="${data.link}" class="btn-light btn-on" target="_blank">view</a>
+            <a href="${data.link}" class="btn-light btn-on" target="_blank">Visualizar</a>
         </div>
     </div>
 `)
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,9 @@ export default (`
         <nav class="nav-menu">
             <a href="/" id="logo-menu">Thiago Britto</a>
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about.html">About</a></li>
-                <li><a href="/projects.html">Projects</a></li>
+                <li><a href="/">Início</a></li>
+                <li><a href="/about.html">Sobre</a></li>
+                <li><a href="/projects.html">Portfólio</a></li>
             </ul>
         </nav>
         <div class="overlay-menu"></div>
