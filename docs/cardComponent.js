c316e395162ce5e29eeb60f3a1d74280b19826ac
thiagobritto <thiagobritto.developer@gmail.com>
2022-06-07 08:11:49 -0300

    inserindo novo item a galeria

--- docs/cardComponent.js
+++ docs/cardComponent.js
@@ -5,7 +5,7 @@ export default data => (`
         </div>
         <div class="descricao">
             <h2>${data.title}</h2>
-            <p>${data.label}</p>
+            <p>${wordCounter(data.label)}</p>
             <div class="h3">stacks</div>
             <div class="stacks">
                 ${stacks(data.stacks)}
@@ -18,4 +18,15 @@ function stacks(stack){
     return stack.map( element => `
         <img width="32" src="assets/icons/svg/${element}.svg" alt="${element}" title="${element}">
     `).join('');
+}
+
+function wordCounter(word, limit = 20)
+{
+    const wordCollection = word.split(' ')
+    if (wordCollection.length > limit){
+        return wordCollection.slice(0, limit).join(' ') + ' ...'
+    } else {
+        return wordCollection.slice(0, limit).join(' ')
+    }
+
 }
